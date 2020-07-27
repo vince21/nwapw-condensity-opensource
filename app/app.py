95a96eb800e17b0679d4defff355b38a1c0ec1e9
vince21 <vincewils11@gmail.com>
2020-07-26 21:21:46 -0700

    basic error handling

--- app/app.py
+++ app/app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, redirect, request
+from flask import Flask, render_template, redirect, request, url_for
 from article import Summarizer
 import gunicorn
 
@@ -14,6 +14,13 @@ def results():
     if request.method == 'POST':
         text = request.form['text']
         percent = request.form['percent']
+
+        #catches empty inputs
+        if not text:
+            return render_template('index.html', errormsg="Please enter text or a link")
+        if not percent:
+            return render_template('index.html', errormsg="Please enter a reduction percentage")
+
         # catches empty percent field
         if percent.isnumeric():
             percent = int(percent) / 100
@@ -28,5 +35,17 @@ def results():
         return render_template('index.html')
 
 
+@app.errorhandler(404)
+def page_not_found(e):
+    # note that we set the 404 status explicitly
+    return render_template('404.html'), 404
+
+@app.errorhandler(500)
+def internal_error(e):
+    # note that we set the 404 status explicitly
+    return render_template('500.html'), 500
+
+
+
 if __name__ == '__main__':
-   app.run(host='0.0.0.0')
+   app.run(host='0.0.0.0', debug=True)
