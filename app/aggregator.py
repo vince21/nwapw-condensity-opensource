60809a7e9d1e32e2e8f48d5b258c027dd9bd2e63
vince21 <vincewils11@gmail.com>
2020-07-27 17:18:31 -0700

    always aggregate

--- app/aggregator.py
+++ app/aggregator.py
@@ -1,17 +1,16 @@
 from newsapi import NewsApiClient
 from article import Summarizer
 import shelve
+import time
 
 if __name__ == '__main__':
+    while True:
     newsapi = NewsApiClient(api_key='1f9393d3ba9d40c3832e87d9088b45cf')
-
-
     top_headlines = newsapi.get_top_headlines(language='en',
                                               country='us')
     articles = []
     news_db = shelve.open('news.db')
     news_db.clear()
-
     for article in top_headlines['articles']:
         try:
             summarizer = Summarizer(article['url'])
@@ -26,3 +25,4 @@ if __name__ == '__main__':
 
     news_db['data'] = articles
     news_db.close()
+    time.sleep(10800)
